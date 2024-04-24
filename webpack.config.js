3b7e3f0aeffdc52354c855d0f85f4ba7152c5cba
Luna Futures <luna.futures+github@gmail.com>
2024-04-24 01:01:53 -0500

    Add typescript changes and webpack support

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,29 @@ const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-	entry: "./src/bootstrap.js",
+	entry: "./src/index.ts",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bootstrap.js",
 	},
 	mode: "development",
 	plugins: [
-		new CopyWebpackPlugin(['src/index.html']),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, "src", "index.html"),
+					to: path.resolve(__dirname, "dist", "index.html")
+				},
+			],
+		}),
 	],
+	module: {
+		rules: [
+			{
+				test: /\.ts$/i,
+				use: 'ts-loader',
+				include: [path.resolve(__dirname, 'src')],
+			}
+		]
+	},
 };
