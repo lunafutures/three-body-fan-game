4e04c8c0e089a9bf5892d5b0b8c1da9e8fd6659d
Luna Futures <luna.futures+github@gmail.com>
2024-04-26 19:37:40 -0500

    Use wasm package as a dependency

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("This is index.ts");
+import async_wasm_three_body from 'wasm-three-body/wasm_three_body';
+import async_wasm_three_body_bg from "wasm-three-body/wasm_three_body_bg.wasm"
+
+async function main() {
+	const wasm_three_body = await async_wasm_three_body;
+	const wasm_three_body_bg  = await async_wasm_three_body_bg;
+}
+
+window.addEventListener("DOMContentLoaded", main, false);
--- webpack.config.js
+++ webpack.config.js
@@ -33,4 +33,7 @@ module.exports = {
 			}
 		]
 	},
+	experiments: {
+		asyncWebAssembly: true,
+	},
 };
